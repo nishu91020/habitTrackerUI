25219e792683a36475c8acb7fd315ab43d713ab0
Nishu Rai <rainishu111@gmail.com>
2025-03-16 12:01:54 +0530

    update

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,10 @@
-import { TaskModal } from '@/app/components/task';
-import '../src/app/globals.css';
-import { useContext, useEffect, useState } from 'react';
-import { useAuth } from './AuthContext';
-import { addTask, getTasks } from '../service/apiService';
+import { TaskModal } from "@/app/components/task";
+import "../src/app/globals.css";
+import { useEffect, useState } from "react";
+import { useAuth } from "./AuthContext";
+import { addTask, deleteTask, getTasks, updateUserTask } from "../service/apiService";
+import { FaRegEdit } from "react-icons/fa";
+import { MdDelete, MdDoneOutline } from "react-icons/md";
 
 interface Task {
   id: string;
@@ -19,27 +21,38 @@ export default function Dashboard() {
   const context = useAuth();
 
   const getUserTasks = async () => {
-    try{
-      console.log('Token:', context.token);
+    try {
+      console.log("Token:", context.token);
       const res = await getTasks(context.token);
-      console.log('Tasks:', res?.data);
+      console.log("Tasks:", res?.data);
       setTasks(res?.data);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
     }
-    catch(error){
-      console.error('Error fetching tasks:', error);
+  };
+
+  const updateTask = async (updatedTask: Task) => {
+    try {
+      await updateUserTask(updatedTask, context.token);
+      await getUserTasks();
+    } catch (error) {
+      console.log("Error updating task:", error);
     }
-  }
-  useEffect(() => {
-    getUserTasks();
-  },[]);
-  
-  const markAsCompleted = (taskId: string) => {
-    setTasks(tasks.map(task => 
-      task.id === taskId ? { ...task, status: 'Completed' } : task
-    ));
+  };
+
+  const markAsCompleted = (updatedTask: Task) => {
+    updateTask({...updatedTask, isCompleted: true});
+    setTasks(
+      tasks.map((task) =>
+        task.id === updatedTask.id ? { ...task, status: "Completed" } : task
+      )
+    );
   };
 
   const handleOpenModal = (task: Task | null) => {
+    if(task){
+      task.dueDate = task.dueDate.split("T")[0];
+    }
     setCurrentTask(task);
     setIsModalOpen(true);
   };
@@ -49,19 +62,32 @@ export default function Dashboard() {
     setCurrentTask(null);
   };
 
-  const handleSaveTask = async(task: Task) => {
-    try{
-      await addTask(task,context.token, context.user);
+  const handleSaveTask = async (task: Task) => {
+    try {
+      await addTask(task, context.token, context.user);
       await getUserTasks();
-    }catch(error){
-      console.log('Error saving task:', error);
+    } catch (error) {
+      console.log("Error saving task:", error);
     }
   };
 
+  const handleDelete = async (taskId: string) => {
+    try {
+      await deleteTask(taskId, context.token);
+      await getUserTasks();
+    } catch (error) {
+      console.log("Error deleting task:", error);
+    }
+  };
+
+  useEffect(() => {
+    getUserTasks();
+  }, []);
+  
   return (
     <div className="min-h-screen bg-gray-100 p-8">
       <div className="flex justify-between items-center mb-6">
-        <h1 className="text-3xl font-bold">{context.user}'s Dashboard  </h1>
+        <h1 className="text-3xl font-bold">{context.user}'s Dashboard </h1>
         <button
           onClick={() => handleOpenModal(null)}
           className="px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-500"
@@ -70,31 +96,49 @@ export default function Dashboard() {
         </button>
       </div>
       <div className="space-y-4">
-        {tasks.map(task => (
+        {tasks.map((task) => (
           <div key={task.id} className="p-4 bg-white rounded-lg shadow-md">
             <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between">
               <div className="flex-1">
                 <h2 className="text-xl font-bold">{task.name}</h2>
                 <p className="text-gray-700">{task.description}</p>
-                <p className="text-gray-500">Due Date: {task.dueDate.split('T')[0]}</p>
-                <p className={`text-sm ${task.isCompleted === true ? 'text-green-500' : 'text-red-500'}`}>
-                  Status: {task.isCompleted ? 'Completed' : 'Pending'}
+                <p className="text-gray-500">
+                  Due Date: {task.dueDate.split("T")[0]}
+                </p>
+                <p
+                  className={`text-sm ${
+                    task.isCompleted === true
+                      ? "text-green-500"
+                      : "text-red-500"
+                  }`}
+                >
+                  Status: {task.isCompleted ? "Completed" : "Pending"}
                 </p>
               </div>
               {!task.isCompleted && (
                 <button
-                  onClick={() => markAsCompleted(task.id)}
-                  className="mt-2 lg:mt-0 lg:ml-4 px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-500"
+                  onClick={() => markAsCompleted(task)}
+                  className="mt-2 lg:mt-2 lg:mt-2 px-4 py-2 text-white bg-green-600 rounded-md hover:bg-green-700 focus:outline-none focus:ring focus:ring-green-500"
                 >
-                  Mark as Completed
+                  <MdDoneOutline />
                 </button>
               )}
+              <button className="mt-2 lg:mt-0 lg:ml-4 px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-500">
+                <FaRegEdit onClick={() => handleOpenModal(task)} />
+              </button>
+              <button className="mt-2 lg:mt-0 lg:ml-4 px-4 py-2 text-white bg-red-600 rounded-md hover:bg-red-700 focus:outline-none focus:ring focus:ring-red-500">
+                <MdDelete onClick={() => handleDelete(task.id)} />
+              </button>
             </div>
           </div>
         ))}
       </div>
       {isModalOpen && (
-        <TaskModal task={currentTask} onClose={handleCloseModal} onSave={handleSaveTask} />
+        <TaskModal
+          task={currentTask}
+          onClose={handleCloseModal}
+          onSave={handleSaveTask}
+        />
       )}
     </div>
   );
--- service/apiService.js
+++ service/apiService.js
@@ -1,81 +1,95 @@
-import axios from 'axios';
-import moment from 'moment';
+import axios from "axios";
+import moment from "moment";
 
 const baseUrl = "http://localhost:8080";
 
 const axiosInstance = axios.create({
-    baseURL: baseUrl,
-    maxRedirects: 3 
+  baseURL: baseUrl,
+  maxRedirects: 3,
 });
 
 export const getTasks = async (token) => {
-    try {
-        const res = axios.get(baseUrl + "/api/tasks", {
-            headers: {
-                token
-            }
-        });
-        return res;
-    } catch (err) {
-        console.log('error fetching tasks for user', err);
-    }
-}
+  try {
+    const res = axios.get(baseUrl + "/api/tasks", {
+      headers: {
+        token,
+      },
+    });
+    return res;
+  } catch (err) {
+    console.log("error fetching tasks for user", err);
+  }
+};
 
 export const addTask = async (task, token, user) => {
-    try {
-        const taskToSave = {
-            id: task.id,
-            name: task.name,
-            description: task.description,
-            createdAt: moment().format('YYYY-MM-DDTHH:mm:ss'),
-            dueDate: moment(task.dueDate).format('YYYY-MM-DDTHH:mm:ss'),
-            isCompleted: false,
-            username: user
-        }
-        const res = axios.post(baseUrl + "/api/tasks", taskToSave,{
-            headers: {
-                token
-            }
-        });
-        return res;
-    } catch (err) {
-        console.log('error adding task', err);
-    }
-}
+  try {
+    const taskToSave = {
+      id: task.id,
+      name: task.name,
+      description: task.description,
+      createdAt: moment().format("YYYY-MM-DDTHH:mm:ss"),
+      dueDate: moment(task.dueDate).format("YYYY-MM-DDTHH:mm:ss"),
+      isCompleted: false,
+      username: user,
+    };
+    const res = axios.post(baseUrl + "/api/tasks", taskToSave, {
+      headers: {
+        token,
+      },
+    });
+    return res;
+  } catch (err) {
+    console.log("error adding task", err);
+  }
+};
 
-export const updateTask = async (task) => {
-    try {
-        const res = axios.put(baseUrl + "/api/tasks", task);
-        return res;
-    } catch (err) {
-        console.log('error updating task', err);
-    }
-}
+export const deleteTask = async (taskId, token) => {
+  try {
+    const res = await axios.delete(baseUrl + `/api/tasks/${taskId}`, {
+      headers: {
+        token,
+      },
+    });
+  } catch (err) {
+    console.log("error deleting task", err);
+  }
+};
+
+export const updateUserTask = async (task, token) => {
+  try {
+    const res = axios.put(baseUrl + `/api/tasks/${task.id}`, task);
+    return res;
+  } catch (err) {
+    console.log("error updating task", err);
+  }
+};
 
 export const login = async (username, password, loginContext) => {
-    const user = {
-        username: username,
-        password: password
-    }
-    console.log('user', user);
-    try {
-        const res = await axiosInstance.post(baseUrl + "/auth/login", user, {validateStatus: status => status >= 200 && status < 303});
-        console.log('res', res.data);
-        return res;
-    } catch (err) {
-        console.log('error logging in', err);
-    }
-}
+  const user = {
+    username: username,
+    password: password,
+  };
+  console.log("user", user);
+  try {
+    const res = await axiosInstance.post(baseUrl + "/auth/login", user, {
+      validateStatus: (status) => status >= 200 && status < 303,
+    });
+    console.log("res", res.data);
+    return res;
+  } catch (err) {
+    console.log("error logging in", err);
+  }
+};
 
 export const signup = async (username, password) => {
-    const user = {
-        username: username,
-        password: password
-    }
-    try {
-        const res = axios.post(baseUrl + "/auth/signup", user);
-        return res;
-    } catch (err) {
-        console.log('error signing up', err);
-    }
-}
+  const user = {
+    username: username,
+    password: password,
+  };
+  try {
+    const res = axios.post(baseUrl + "/auth/signup", user);
+    return res;
+  } catch (err) {
+    console.log("error signing up", err);
+  }
+};
