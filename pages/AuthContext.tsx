4afd4ac961d2ac44692881f3f984cf7e10bdfb08
Nishu Rai <rainishu111@gmail.com>
2025-03-23 13:46:00 +0530

    added session storage

--- pages/AuthContext.tsx
+++ pages/AuthContext.tsx
@@ -1,40 +1,63 @@
 import { jwtDecode } from "jwt-decode";
 import { createContext, ReactNode, useContext, useState } from "react";
+import { useEffect } from "react";
 
 interface AuthContextType {
-    token: string | null;
-    user: string | null;
-    login: (token: string) => void;
-    logout: () => void;
+  token: string | null;
+  user: string | null;
+  login: (token: string) => void;
+  logout: () => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-    const [token, setToken] = useState<string | null>(null);
-    const [user, setUser] = useState<string | null>(null);
-    
-    const login = (token: string) => {
-        console.log('login context token', token);
-        setToken(token);
+  const getInitialState = () => {
+    if (typeof window !== "undefined") {
+    const currentToken = sessionStorage.getItem("token");
+    return currentToken ? currentToken : null;
+    }
+    return null;
+  };
+  const [token, setToken] = useState<string | null>(getInitialState);
+  const [user, setUser] = useState<string | null>(null);
+
+  const setCurrentUser = (token: string | null) => {
+    if (token) {
         const decodedToken = jwtDecode(token);
         const sub = decodedToken.sub as string | undefined;
         setUser(sub ?? null);
-    }
-    const logout = () => {
-        setToken(null);
+    } else {
         setUser(null);
     }
-    return (
-        <AuthContext.Provider value={{user, token, login, logout }}>
-            {children}
-        </AuthContext.Provider>
-    ); 
-}
+  }
+
+  useEffect(() => {
+    if(typeof window !== "undefined") {
+        sessionStorage.setItem("token", token ?? "");
+        setCurrentUser(token);
+    }
+  }, [user]);
+
+  const login = (token: string) => {
+    console.log("login context token", token);
+    setToken(token);
+    setCurrentUser(token);
+  };
+  const logout = () => {
+    setToken(null);
+    setUser(null);
+  };
+  return (
+    <AuthContext.Provider value={{ user, token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export const useAuth = (): AuthContextType => {
-    const context = useContext(AuthContext);
-    if (context === undefined) {
-        throw new Error('useAuth must be used within an AuthProvider');
-    }
-    return context;
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
 };
